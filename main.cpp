f90c3eca2c25a7ca28d274cb575512eefbb5d13c
shazzdev <81687930+shazzdev@users.noreply.github.com>
2023-01-09 13:12:27 +0530

    replaced string input with cmd, added a search feature.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 using namespace std;
-
+void search () {
+    ifstream input;
+	size_t pos;	string line;
+	string SEARCH_WORD;
+	input.open("student_info.txt");
+	if (input.is_open())
+	{
+		cin >> SEARCH_WORD;
+		while (getline(input, line))
+		{
+			pos = line.find(SEARCH_WORD);
+			if (pos != string::npos) // string::npos is returned if string is not found
+			{
+				cout << "Found "<<SEARCH_WORD<<endl;
+				break;
+			}
+		}
+	}
+}
 int main()
 {
     for (;;)
     {
         cout << "$ " << flush;
-        string input;
-        getline(cin, input);
-        cout << endl;
+        string cmd;
+        string word;
+        getline(cin, cmd);
+        if cmd==("search")
+            search();
         if (input == "kill")
         break;
     }
