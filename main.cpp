0da499a2ae41bbf06746c7d95e3525a6d63648d5
shazzdev <81687930+shazzdev@users.noreply.github.com>
2023-01-14 03:46:49 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 using namespace std;
-void search () {
-    ifstream input;
-	size_t pos;	string line;
+int display()
+{
+	int Name = 0;
+	int Standard = 0;
+	int Division = 0;
+	int Class = 0;
+	cout << "\nName\tStandard\tDivision\tClass\n";
+	cout << Name <<"\t"<< Standard<<"\t\t"<< Division<< "\t\t"<< Class<<endl;
+};
+int search()
+{
+	ifstream input;
+	size_t pos;
+	string line;
 	string SEARCH_WORD;
+
 	input.open("student_info.txt");
 	if (input.is_open())
 	{
+		cout << "Whom do you want to search? ";
 		cin >> SEARCH_WORD;
 		while (getline(input, line))
 		{
 			pos = line.find(SEARCH_WORD);
 			if (pos != string::npos) // string::npos is returned if string is not found
 			{
-				cout << "Found "<<SEARCH_WORD<<endl;
-				break;
+				cout << "Found " << SEARCH_WORD;
+				display();
 			}
 		}
-	}
-}
-int main()
-{
-    for (;;)
-    {
-        cout << "$ " << flush;
-        string cmd;
-        string word;
-        getline(cin, cmd);
-        if cmd==("search")
-            search();
-        if (input == "kill")
-        break;
     }
-    return 0;
+}
+
+int main()
+    for (;;)	{
+	cout << "$ " << flush;
+	string cmd;
+	string word;
+		getline(cin, cmd);
+		if (cmd == "kill")
+			break;
+		if (cmd == "search")
+			search();
+	}
+	return 0;
 }
